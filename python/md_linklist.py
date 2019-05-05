9bcb787a8da00fba8ffcf4562bc6c447e5ab3eb4
Isaac Friedman <isaac@isaac-friedman.com>
2019-05-05 08:24:24 -0400

    refactor: Minimized file I/O.

--- python/md_linklist.py
+++ python/md_linklist.py
@@ -2,9 +2,14 @@ print("Welcome to the Markdown link list generator!")
 filename = raw_input("What will your file be called? ")
 count = int(raw_input("How many links will be in your list? "))
 
+links = []
+urls = []
+
+for i in range(count): #Placeholders for link texts
+    links.append('[]' + '[' + str(i+1) + ']  ')
+    urls.append('[' + str(i+1) + ']' + ': <url here>  ')  #Placeholders for link URLS
+
 with open('./'+ filename, 'wt') as outfile:
-    for i in range(count): #Placeholders for link texts
-        outfile.write('[]' + '[' + str(i+1) + ']  \n')
+    outfile.write('\n'.join(str(link) for link in links))
     outfile.write('\n\n') #spacer
-    for i in range(count):
-        outfile.write('[' + str(i+1) + ']' + ': <url here>  \n')  #Placeholders for link URLS
+    outfile.write('\n'.join(str(url) for url in urls))
